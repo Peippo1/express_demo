a8e0452a1561a1044ad217b1d47c44971a8baa50
Peippo <32274695+Peippo1@users.noreply.github.com>
2021-01-31 21:57:58 +0000

    commit to master branch demo 1

---
+++ src/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const { myFunction, myOtherFunction } = require("./middleware/auth");
+
+const app = express();
+const public_directory = path.join(__dirname, "../public");
+
+app.use(express.json());
+app.use(myFunction);
+app.use(myOtherFunction);
+app.use(express.static(public_directory));
+
+app.get("/data", (req, res) => {
+  console.log(req.query);
+  res.send({ message: `Hello, your name is ${req.query.name}` });
+});
+
+app.get("/something", (req, res) => {
+  res.send({ name: "Shiela", age: 42 });
+});
+
+app.get("/dog", (req, res) => {
+  res.send("woof");
+});
+
+app.get("/dog/:id", (req, res) => {
+  console.log(req.params);
+  res.send("success");
+});
+
+app.get("/person/:id", (req, res) => {
+  console.log(req.params.id);
+  res.send("random");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send({ data: req.body });
+});
+
+app.post("/task", (req, res) => {
+  console.log(req.body);
+  fs.writeFileSync("tasks.txt", req.body.task);
+  res.send("success");
+});
+
+app.listen(5000, () => {
+  console.log("listening on port 5000");
+});
